d49e88dcc36370fbf61503474e18d8940f5392f7
Adrian27131 <AdrianMontoro9991@gmail.com>
2023-04-04 19:16:27 -0400

    checkpoint

---
+++ let cards = document.js
@@ -0,0 +1,19 @@
+let cards = document.querySelectorAll('.card');
+let pattern = [1, 2, 3, 4, 5, 6, 7, 8];
+let wrongGuesses = 0;
+let flippedCards=[];
+
+
+function flipCard(card) {
+    card.classList.add("flipped");
+}
+
+function checkMatch(){
+let card1 = flippedCards[0];
+let card2 = flippedCards[1];
+}
+
+if (card1.set.pattern === )
+
+cards.forEach(card => card.addEventListener('click', flipCard));
+
