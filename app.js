7b97e61ebdf77173549729a2cf6e454047a522e1
Adrian27131 <AdrianMontoro9991@gmail.com>
2023-04-05 19:21:13 -0400

    game reset logic

--- app.js
+++ app.js
@@ -1,11 +1,11 @@
 const section = document.querySelector('section');
 const playerTriesCount = document.querySelector ('span');
-const playerTries = 3;
+let playerTries = 3;
 playerTriesCount.textContent = playerTries
 
 
 const getData = () => [{}
-,{ imgSrc: "./images/images/03910032-2.jpg", name: "flowerpot1"},
+,{ imgSrc: "./images/images/03940020.jpg", name: "flowerpot1"},
 { imgSrc: "./images/images/03930028.jpg", name: "Park"},  
 { imgSrc: "./images/images/03930028.jpg", name: "Park"}, 
 { imgSrc: "./images/images/03940030.jpg", name: "Waterfall"}, 
@@ -20,6 +20,7 @@ const getData = () => [{}
 { imgSrc: "./images/images/04440034.jpg", name: "pineapple"}, 
 { imgSrc: "./images/images/05460013.jpg", name: "Nikon-Me"},  
 { imgSrc: "./images/images/05460013.jpg", name: "Nikon-Me"}, 
+{ imgSrc: "./images/images/03940020.jpg", name: "flowerpot1"},
 ];
 const randomize = () => {
   const cardData = getData();
@@ -69,9 +70,24 @@ if (flippedCard.length === 2){
     flippedCard.forEach(card => {
       card.classList.remove("flipped");
       setTimeout(() => card.classList.remove("toggleCard"), 1000);
-    })
+    });
+    playerTries--;
+    playerTriesCount.textContent = playerTries; 
+    if(playerTries === 0) {
+      reset();
+    } 
   }
 }
+};
+    
+
+let reset = () => {
+  let cardData = randomize ();
+  let faces = document.querySelectorAll(".face");
+  let cards = document.querySelectorAll('.card');
+cardData.forEach((item,index) => {
+  cards[index].classList.remove('toggleCard');
+});
 };
 
 cardGenerator();
