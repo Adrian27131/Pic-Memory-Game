09a14f6aaba5bf73a3e15ecc6328c8d407505034
Adrian27131 <AdrianMontoro9991@gmail.com>
2023-04-05 11:42:10 -0400

    card details

--- let cards = document.js
+++ let cards = document.js
@@ -13,7 +13,56 @@ let card1 = flippedCards[0];
 let card2 = flippedCards[1];
 }
 
-if (card1.set.pattern === )
+if (card1.set.pattern === card2.set.pattern) {
+card1.removeEventListener('click', handleClick);
+card2.removeEventListener('click', handleClick);
+} else {
+    wrongGuesses++;
+    setTimeout(() => {
+        card1.classList.remove("flipped");
+        card2.classList.remove("flipped");
+    }, 1000);
+}
+{
+flippedCards = [];
+}
+
+function handleClick() {
+    if (flippedCards.lenght < 2) {
+flipCard(this);
+flippedCards.push(this);
+    }
+    if (flippedCards.lenght === 2) {
+        setTimeout(checkMatch, 500);
+    }
+}
+function startGame() {
+    cards,forEach(card => {
+        let pattternIndex = Math.floor(Math.random() * pattern.lenght);
+        card.set.pattern = pattern[pattternIndex];
+        pattern.splice(patternIndex, 1);
+    });
+    cards.forEach(card => {
+        card.addEventListener('click', handleClick)
+
+
+    })
+
+}
+   startGame();
+        
+        
+        
+        
+
+
+
+
+
+
+
+
+
 
 cards.forEach(card => card.addEventListener('click', flipCard));
 
